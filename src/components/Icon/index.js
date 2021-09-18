c02c14b853e34ee0afcad70fb4f9a1d8f63752e5
danielbarion <danielbarionn@gmail.com>
2021-09-18 13:25:15 -0300

    feat: add icon component into project

---
+++ src/components/Icon/index.js
@@ -0,0 +1,25 @@
+/* eslint-disable react/no-danger */
+import React from 'react'
+import PropTypes from 'prop-types'
+import classNames from 'classnames'
+import style from './style.module.css'
+
+const Icon = ({ className, icon, size }) => {
+  const containerClassnames = classNames(className, style.container, style[size], 'material-icons')
+
+  return <i className={containerClassnames}>{icon}</i>
+}
+
+Icon.propTypes = {
+  className: PropTypes.string,
+  size: PropTypes.oneOf(['xxxs', 'xxs', 'xs', 's', 'm', 'l', 'xl', 'xxl', 'xxxl']),
+  icon: PropTypes.string,
+}
+
+Icon.defaultProps = {
+  className: null,
+  size: 'm',
+  icon: null,
+}
+
+export default Icon
